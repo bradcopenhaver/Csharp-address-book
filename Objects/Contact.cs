a494763d18b135b6ec9ea80fc1edd463c3514a38
bradcopenhaver <bradcopenhaver@gmail.com>
2016-11-18 09:19:00 -0800

    create get, set, find, and clear functions for Contact class properties

--- Objects/Contact.cs
+++ Objects/Contact.cs
@@ -24,5 +24,61 @@ namespace AddressBook.Objects
       _instances.Add(this);
       _id = _instances.Count;
     }
+    public string GetFirstName()
+    {
+      return _firstName;
+    }
+    public void SetFirstName(newName)
+    {
+      _firstName = newName;
+    }
+    public string GetLastName()
+    {
+      return _lastName;
+    }
+    public void SetLastName(newName)
+    {
+      _lastName = newName;
+    }
+    public string GetPhone()
+    {
+      return _phone;
+    }
+    public void SetPhone(newPhone)
+    {
+      _phone = newPhone;
+    }
+    public string GetEmail()
+    {
+      return _email;
+    }
+    public void SetEmail(newEmail)
+    {
+      _email = newEmail;
+    }
+    public Address GetAddress()
+    {
+      return _address;
+    }
+    public void SetAddress(newAddress)
+    {
+      _address = newAddress;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public static void GetAllContacts()
+    {
+      return _instances;
+    }
+    public static void ClearAllContacts()
+    {
+      _instances.Clear();
+    }
+    public static Contact Find(searchId)
+    {
+      return _instances[searchId-1];
+    }
   }
 }
