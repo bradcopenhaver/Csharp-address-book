6fd5ed5898c46a64dc370857144d48a359551d79
bradcopenhaver <bradcopenhaver@gmail.com>
2016-11-18 11:16:56 -0800

    create view and route for contact creation confirmation page

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -14,15 +14,14 @@ namespace AddressBook
 
         return View["index.cshtml", allContacts];
       };
-      Post["/"] = _ =>
+      Post["/contact/new"] = _ =>
       {
         Address newAddress = new Address(Request.Form["street"], Request.Form["city"], Request.Form["state"], Request.Form["zip"]);
         Contact newContact = new Contact(Request.Form["firstName"], Request.Form["lastName"], Request.Form["phone"], Request.Form["email"], newAddress);
-        List<Contact> allContacts = Contact.GetAllContacts();
 
-        return View["index.cshtml", allContacts];
+        return View["contact_created.cshtml", newContact];
       };
-      Get["/contact/new"] = _ => View["contact_form.cshtml"];
+      Get["/contact/form"] = _ => View["contact_form.cshtml"];
       Get["/contact/{id}"] = parameters =>
       {
         Contact currentContact = Contact.Find(parameters.id);
